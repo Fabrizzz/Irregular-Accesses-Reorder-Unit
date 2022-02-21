fe3d13ae6171909e6780343e7253e16c49ec32da
giampi97 <giampietro.fabriziobonaccorsi@gmail.com>
2022-02-21 13:58:29 +0100

    Added a few comments

--- GraphGenerator.cpp
+++ GraphGenerator.cpp
@@ -3,58 +3,57 @@
 
 using namespace std;
 
-class GraphGenerator{
+class GraphGenerator {
 public:
-    
-    static vector<vector<int>> create_graph(int n, double density){
-        vector<vector<int>> graph{};
 
-        for (int i = 0; i < n; i++) {
-            graph.push_back({});
-        }
-        int universe[n];
+    static vector<vector<int>> create_random_graph(int n, double density) {//density [0,1]
+        vector<vector<int>> graph(n);
+
+        vector<int> universe(n);
+
         for (int i = 0; i < n; i++) {
             universe[i] = i;
         }
+
         DisjointSet ds = DisjointSet();
-        ds.makeSet(universe, n);
+        ds.makeSet(universe);
+
         for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                if ((double) rand() / (RAND_MAX) < density){
-                    graph[universe[i]].push_back(universe[j]);
-                    graph[universe[j]].push_back(universe[i]);
-                    ds.Union(i,j);
+            for (int j = i + 1; j < n; j++) {
+                //todo change with a better rand function
+                if ((double) rand() / (RAND_MAX) < density) {
+                    graph[i].push_back(j);
+                    graph[j].push_back(i);
+                    ds.Union(i, j);
                 }
             }
         }
-    
-        for (int i = 0; i < n-1; i++) {
-            if (ds.Find(universe[i]) != ds.Find(universe[i+1])){
-                //cout << "sus" << endl;
-                graph[universe[i]].push_back(universe[i+1]);
-                graph[universe[i+1]].push_back(universe[i]);
-                ds.Union(universe[i],universe[i+1]);
+
+        //connect components
+        for (int i = 0; i < n - 1; i++) {
+            if (ds.Find(i) != ds.Find(i + 1)) {
+                graph[i].push_back(i + 1);
+                graph[i + 1].push_back(i);
+                ds.Union(i, i + 1);
             }
         }
 
         return graph;
     }
 
-    void print(int size, vector<vector<int>> graph_0){
+    void print(const vector<vector<int>> &graph_0) {
 
-        for(int i = 0;i < size;i++) {
+        for (int i = 0; i < graph_0.size(); i++) {
             cout << i << " -> ";
-            for (int j = 0; j < graph_0[i].size(); j++)
-                cout << graph_0[i][j] << " ";
+            for (int j : graph_0[i])
+                cout << j << " ";
             cout << endl;
         }
     }
 };
-/*
-int main (){
-    GraphGenerator gg = {};
 
-    vector<vector<int>> graph = gg.create_graph(10000, 0.5);
+/*int main (){
+    create_random_graph(10000, 0.0);
 
     return 0;
 }*/
