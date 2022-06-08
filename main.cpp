a4751da04a2527e87dec8144ee9410c900d5e4d6
fabrizzz <giampietro.fabriziobonaccorsi@gmail.com>
2022-06-08 21:28:56 +0200

    Graphs path directory modified

--- main.cpp
+++ main.cpp
@@ -26,19 +26,19 @@ int main() {
     vector<vector<int>> neighbours = GraphGenerator::create_random_graph(10000, 0.01);
     testBench("RandomGeneratedGraph", neighbours, 0);
     //https://sparse.tamu.edu/Newman/cond-mat-2005
-    neighbours = GraphGenerator::load_graph_matrix_market("../graphs/cond-mat-2005.mtx");
+    neighbours = GraphGenerator::load_graph_matrix_market("./graphs/cond-mat-2005.mtx");
     testBench("cond-mat-2005", neighbours, 1);
     //https://sparse.tamu.edu/Belcastro/human_gene1
-    neighbours = GraphGenerator::load_graph_matrix_market("../graphs/human_gene1.mtx");
+    neighbours = GraphGenerator::load_graph_matrix_market("./graphs/human_gene1.mtx");
     testBench("human_gene1", neighbours, 25);
     //https://sparse.tamu.edu/INPRO/msdoor
-    neighbours = GraphGenerator::load_graph_matrix_market("../graphs/msdoor.mtx");
+    neighbours = GraphGenerator::load_graph_matrix_market("./graphs/msdoor.mtx");
     testBench("msdoor", neighbours, 1);
     //https://www.cc.gatech.edu/dimacs10/archive/delaunay.shtml
-    neighbours = GraphGenerator::load_graph_Metis("../graphs/delaunay_n19.graph");
+    neighbours = GraphGenerator::load_graph_Metis("./graphs/delaunay_n19.graph");
     testBench("delaunay_n19", neighbours, 1);
     //https://www.cc.gatech.edu/dimacs10/archive/kronecker.shtml
-    neighbours = GraphGenerator::load_graph_Metis("../graphs/kron_g500-simple-logn18.graph");
+    neighbours = GraphGenerator::load_graph_Metis("./graphs/kron_g500-simple-logn18.graph");
     testBench("kron_g500-simple-logn18", neighbours, 1);
 
     return 0;
